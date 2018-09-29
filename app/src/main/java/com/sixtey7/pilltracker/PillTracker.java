19540c30763ffc54371f8ef11b163d0d963f1de1
Patrick Shaw <sixtey7@gmail.com>
2018-09-29 16:21:16 -0400

    shell of the project

---
+++ app/src/main/java/com/sixtey7/pilltracker/PillTracker.java
@@ -0,0 +1,13 @@
+package com.sixtey7.pilltracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PillTracker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pill_tracker);
+    }
+}
